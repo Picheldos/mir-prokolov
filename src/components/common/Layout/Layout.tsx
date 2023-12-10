707b219151bfeda69ad443da92958f4dae96b910
blaggjuzxcmr <vasporelsa@gmail.com>
2023-12-11 00:00:35 +0300

    title+favicon

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -49,7 +49,7 @@ const Layout: React.FC<LayoutProps> = ({ children, meta, header, sandwich }) => 
     return (
         <>
             <Head>
-                <title>{meta.title}</title>
+                <title>{'Мир проколов'}</title>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <meta name="description" content={meta.description} />
